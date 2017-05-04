9cf3a8fa0bd87b1199cd7bc9e49fa7ccc534bfc2
yepzy <gamesdjaki@gmail.com>
2017-05-04 08:55:43 +0000

    clean example from cloud9

--- gda.js
+++ gda.js
@@ -1,8 +1,12 @@
 var util = require('util'),
     twitter = require('twitter');
-var twit = new twitter({
-    consumer_key: 'STATE YOUR NAME',
-    consumer_secret: 'STATE YOUR NAME',
-    access_token_key: 'STATE YOUR NAME',
-    access_token_secret: 'STATE YOUR NAME'
+var twit = new twitter();
+var params = {screen_name: 'nodejs'};
+twit.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error) {
+    console.log(tweets);
+  } else {
+      console.log(error);
+  }
 });
+https://twitter.com/login?redirect_after_login=https%3A/apps.twitter.com/
