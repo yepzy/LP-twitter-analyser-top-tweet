a95ff28cadbc76c9235de4e4e4b63be0c55787c7
yepzy <cyril.bid@outlook.com>
2017-05-05 01:21:50 +0200

    update readme + add time elapsed param in config

--- config.js
+++ config.js
@@ -1,23 +1,24 @@
 const env = require('common-env')();
 
 const config = env.getOrElseAll({
-    twitter: {
-        consumer_key: {
-            $default: 'JICzlMMuDvjdh5yf74xnIrv39'
-        },
-        consumer_secret: {
-            $default: 'O3WmxES0Tl9vplK5NmvdYF7Cvh6y6d633pRWfT5mM9KCyvc96a'
-        },
-        access_token_key: {
-            $default: '619461847-LXLrvM1H6iOBmUhJzUoRhQVyWdcFuqDr5XIVIwfx'
-        },
-        access_token_secret: {
-            $default: 't07Db3jmDF2qihhwlncMgIaj9eXMEbHzpt8kr0vdqD0zp'
-        }
-    },
+    //twitter: {
+    //    consumer_key: {
+    //        $default: 'JICzlMMuDvjdh5yf74xnIrv39'
+    //    },
+    //    consumer_secret: {
+    //        $default: 'O3WmxES0Tl9vplK5NmvdYF7Cvh6y6d633pRWfT5mM9KCyvc96a'
+    //    },
+    //    access_token_key: {
+    //        $default: '619461847-LXLrvM1H6iOBmUhJzUoRhQVyWdcFuqDr5XIVIwfx'
+    //    },
+    //    access_token_secret: {
+    //        $default: 't07Db3jmDF2qihhwlncMgIaj9eXMEbHzpt8kr0vdqD0zp'
+    //    }
+    //},
     RABBITMQ_URL: 'amqp://sacuqjih:omOG4lRsjCl-5Rn2-slRUjuHYmSGzxRf@lark.rmq.cloudamqp.com/sacuqjih',
     NB_MAX_TOP_TWEET: 10,
-    LOG_FILE: 'top_tweet.log'
+    LOG_FILE: 'top_tweet.log',
+    MAX_TIME_ELAPSED: '1h'
 });
 
 module.exports = config;
