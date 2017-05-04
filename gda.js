4bc665ba02277c0dc119d87b78171742bb19bfe5
yepzy <gamesdjaki@gmail.com>
2017-05-04 08:28:30 +0000

    first commit from cloud9

---
+++ gda.js
@@ -0,0 +1,8 @@
+var util = require('util'),
+    twitter = require('twitter');
+var twit = new twitter({
+    consumer_key: 'STATE YOUR NAME',
+    consumer_secret: 'STATE YOUR NAME',
+    access_token_key: 'STATE YOUR NAME',
+    access_token_secret: 'STATE YOUR NAME'
+});
