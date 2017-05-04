7a0ae0b9a10bf9b32ec972a1d4485b562b2b0813
yepzy <gamesdjaki@gmail.com>
2017-05-04 08:59:25 +0000

    adding test rabbitmq

--- gda.js
+++ gda.js
@@ -1,12 +1,89 @@
-var util = require('util'),
-    twitter = require('twitter');
-var twit = new twitter();
-var params = {screen_name: 'nodejs'};
-twit.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-    console.log(tweets);
-  } else {
-      console.log(error);
+const _ = require('lodash');
+
+function Bus(){
+  const _lapinou = {};
+
+  function _ensureEventExist(eventName){
+    if(!_lapinou[eventName]){
+      _lapinou[eventName] = {callbacks:[]};
+    }
   }
+
+  function send(eventName, message){
+    _ensureEventExist(eventName);
+    _lapinou[eventName].callbacks.forEach(function(f){
+      f._queue.push(message);
+      f._loadNext();
+    });
+  }
+
+  function _initConsumer(consumer){
+    consumer._isConsuming = false;
+    consumer._queue = [];
+
+
+    consumer._loadNext = function(){
+      if(consumer._isConsuming){
+        return;
+      }
+
+      if(consumer._queue.length > 0){
+        consumer._isConsuming = true;
+        const nextMessage = _.extend({}, consumer._queue.pop());
+        nextMessage.handle = consumer._handle;
+        consumer(nextMessage);
+        return;
+      }
+
+      consumer._isConsuming = false;
+    };
+
+    consumer._handle = {
+      getQueueSize: function(){
+        return consumer._queue.length;
+      },
+      acknowledge:function(){
+        consumer._isConsuming = false;
+        consumer._loadNext();
+      }
+    };
+
+    return consumer;
+  }
+
+  function listen(eventName, options, f){
+    _ensureEventExist(eventName);
+    _lapinou[eventName].callbacks.push(_initConsumer(f));
+  }
+
+  return {
+    send: send,
+    listen: listen,
+  };
+}
+
+const bus = Bus();
+
+
+setInterval(function() {
+  bus.send('my.event', {
+    server: 'yolo',
+    metric: Math.floor(Math.random() * 1000),
+    createdAt: + new Date()
+  });
+}, 100);
+
+
+bus.listen('my.event', {ack: true}, function logger(event) {
+  console.log('B',event.handle.getQueueSize(), event);
+  setTimeout(function(){
+    event.handle.acknowledge();
+  }, 500);
 });
-https://twitter.com/login?redirect_after_login=https%3A/apps.twitter.com/
+
+bus.listen('my.event', {ack: true}, function alerting(event) {
+  console.log('A',event.handle.getQueueSize(),  event);
+  setTimeout(function(){
+      event.handle.acknowledge();
+  }, 1000);
+});
